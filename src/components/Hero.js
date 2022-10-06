35c451783ab0da6ff4c9096b2c7095be47a52b2f
janethxuiz <janethruiz152@gmail.com>
2022-10-05 19:09:24 -0700

    added nav and home

---
+++ src/components/Hero.js
@@ -0,0 +1,21 @@
+import './HeroStyles.css';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='content'>
+            <p>HI MY NAME IS JANETH</p>
+            <h1>WELCOME</h1>
+        <div>
+            <Link to='/project' className='btn'>Projects</Link>
+            <Link to='/contact' className='btn btn-light'>Contact</Link>
+        </div>
+        </div>
+    </div>
+  );
+};
+
+export default Hero;
