bed725688673b83930eec0508e7e356878842d48
janethxuiz <janethruiz152@gmail.com>
2022-10-26 13:47:26 -0700

    formatted home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Hero from "../components/Hero"
-import Footer from "../components/Footer"
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar/>
-            <Hero/>
-            <Footer/>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
