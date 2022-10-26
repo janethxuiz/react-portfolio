7041c74870476e59a4d50df69d9742d30714576f
janethxuiz <janethruiz152@gmail.com>
2022-10-26 13:29:01 -0700

    setting navbar fixed when scorlled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import './NavbarStyles.css'
 
-import React from 'react'
+import React, { useState} from 'react'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
+    const [color, setColor] = useState(false);
+    const changeColor = () => {
+        if(window.scrollY >= 100){
+            setColor(true);
+        }else{
+            setColor(false);
+        }
+    };
+
+    window.addEventListener("scroll", changeColor)
+
   return (
-    <div className="header">
+    <div className={color ? 'header header-bg': 'header'}>
         <Link to='/'>
             <h1>Portfolio</h1>
         </Link>
